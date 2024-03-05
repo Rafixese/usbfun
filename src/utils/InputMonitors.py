fecbdb3260b5541812082b804a917af34fd25a38
Rafał Hrabia <rafixese@gmail.com>
2024-03-05 18:41:04 +0100

    Put monitor in utils, cleanin up

---
+++ src/utils/InputMonitors.py
@@ -0,0 +1,38 @@
+from typing import Callable, Union
+
+from pynput import keyboard
+from pynput.keyboard import HotKey
+from pynput.keyboard._base import Listener, KeyCode, Key
+
+
+class HotkeyMonitor:
+    def __init__(self):
+        self.__hotkeys: list[HotKey] = []
+        self.__listener: Listener = keyboard.Listener(
+            on_press=self.__handle_press,
+            on_release=self.__handle_release
+        )
+
+    def add_hotkey(self, shortcut: str, handler: Callable[[KeyCode], None]):
+        self.__hotkeys.append(
+            keyboard.HotKey(
+                keyboard.HotKey.parse(shortcut),
+                handler)
+        )
+
+    def __handle_press(self, key: Union[KeyCode, Key]):
+        for hk in self.__hotkeys:
+            hk.press(self.__listener.canonical(key))
+
+    def __handle_release(self, key: Union[KeyCode, Key]):
+        for hk in self.__hotkeys:
+            hk.release(self.__listener.canonical(key))
+
+    def start(self):
+        self.__listener.start()
+
+    def stop(self):
+        self.__listener.stop()
+
+    def join(self):
+        self.__listener.join()
